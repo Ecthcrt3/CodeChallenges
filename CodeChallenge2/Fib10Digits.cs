e35a1373c1ab2100c7dc32c52981f2c85638c04c
Gene Cathcart <ecthcrt3@gmail.com>
2023-07-12 08:34:10 -0500

    finished codeChallenge 2

---
+++ CodeChallenge2/Fib10Digits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge2
+{
+    public class Fib10Digits
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            int value1 = 1;
+            int value2 = 1;
+            int temp = 0;
+
+            do
+            {
+                index++;
+
+                temp = value2;
+                value2 += value1;
+                value1 = temp;
+            } while (value2 < 100000000);
+
+            Console.WriteLine($"The index of the first number with 10 digits is {index} ");
+        }
+    }
+}
---
+++ CodeChallenge2/FibEvenSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge2
+{
+    public class FibEvenSum
+    {
+        static void Main(string[] args)
+        {
+            int evenSum = 0;
+            int value1 = 1;
+            int value2 = 2;
+            int temp = 0;
+
+            while(value2 < 4000000)
+            {
+                if(value2 % 2 == 0)
+                {
+                    evenSum += value2;
+                }
+
+                temp = value2;
+                value2 += value1;
+                value1 = temp;
+            }
+
+            Console.WriteLine(evenSum);
+        }
+
+    }
+}
